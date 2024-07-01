262aef0c6cb76ac059493e18b34db2f7634f11d7
letelumiere <letelumiere@gmail.com>
2024-07-01 14:17:23 +0900

    commits.

--- lib/item_basket_page.dart
+++ lib/item_basket_page.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/models/product.dart';
+import 'package:flutter_practice/services/cart_services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'constants.dart';
 
 class ItemBasketPage extends StatefulWidget {
   const ItemBasketPage({super.key});
@@ -8,8 +13,35 @@ class ItemBasketPage extends StatefulWidget {
 }
 
 class _ItemBasketPageState extends State<ItemBasketPage> {
+  late CartService cartService;
+  Map<String, dynamic> cartMap = {};
+  List<int> keyList = [];
+  Stream<QuerySnapshot<Product>>? productList;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCartService();
+  }
+
+  Future<void> _initializeCartService() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    cartService = CartService(sharedPreferences: sharedPreferences);
+
+    setState(() {
+      cartMap = cartService.getCartMap();
+      keyList = cartService.getKeyList(cartMap);
+      final productListRef = cartService.getProductListRef(keyList);
+      productList = cartService.getProductListStream(productListRef);
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("임시 창")),
+
+    );
   }
-}
+  }
+
